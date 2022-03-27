38d859c0f6c7a93572be9d0be3ac83380c1d347c
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-03-27 10:31:47 +0200

    [:construction:] trabajando en Item

---
+++ src/main/java/edu/poniperro/items/Item.java
@@ -0,0 +1,47 @@
+package edu.poniperro.items;
+
+public class Item implements Product {
+    //Métodos
+    //package , no es necesario poner nada
+    String name;
+    Double price;
+    String extra;
+
+    // Contructor suelen ser públicos
+    public Item(String contructorName, Double contructoPrice){
+        this.name = contructorName;
+        this.price = contructoPrice;
+    }
+    public Item(String contructorName, Double contructoPrice, String contructorExtra){
+        this.name = contructorName;
+        this.price = contructoPrice;
+        this.extra = contructorExtra;
+    }
+
+    //Sobreescribe
+    @Override
+    public String name() {
+        return this.name;
+    }
+
+    @Override
+    public Double price() {
+        return this.price;
+    }
+
+    @Override
+    public String extra() {
+        return this.extra;
+    }
+
+    @Override
+    public Boolean isRegular() {
+        return null;
+    }
+
+    //@Override con el toString siempre tienen que ir unido
+    @Override
+    public String toString(){
+        return name() + "...." + price().toString() + "$";
+    }
+}
